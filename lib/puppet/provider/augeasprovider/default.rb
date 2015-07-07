d5abc2f5c1e246da404a3ec02c8bda6ceb54c597
Raphaël Pinson <raphink@gmail.com>
2015-07-07 21:41:45 +0200

    Avoid shadowing sp

--- lib/puppet/provider/augeasprovider/default.rb
+++ lib/puppet/provider/augeasprovider/default.rb
@@ -250,7 +250,7 @@ Puppet::Type.type(:augeasprovider).provide(:default) do
             else
               sp = "#{p}/#{sublabel}"
             end
-            aug.match(sp).map { |sp| aug.get(sp) }
+            aug.match(sp).map { |spp| aug.get(spp) }
           end
         end.flatten
       when :hash
